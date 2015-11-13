553b2a4abd3bb7f0e2d36988ceb9465fbd484326
Omar Saleh <omarsaleh0106@gmail.com>
2015-11-13 18:57:28 +0200

    added the remaining migrations and modifies the existing ones

--- db/migrate/20151112105534_create_posts.rb
+++ db/migrate/20151112105534_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.integer :user_id
-      t.meta_data :pic
+      t.integer :tag_id
 
       t.timestamps null: false
     end
