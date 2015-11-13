01f5d4247eab1e02c572a244961219e3e35d9807
Omar Saleh <omarsaleh0106@gmail.com>
2015-11-13 19:40:48 +0200

    added intheritance relationships:

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,2 +1,9 @@
 class Status < ActiveRecord::Base
+	validate :that_atleast_one_field_is_present
+
+	def that_atleast_one_field_is_present
+		if self.description.nil? and self.pic.nil?
+			errors.add(:description , "You have to upload a picture or write a status!")
+		end
+	end
 end
