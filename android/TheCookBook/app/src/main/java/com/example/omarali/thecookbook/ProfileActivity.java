2e417fc324f05904f008a197c59be1a32515a416
Omar Ali <omar.moh.aly@gmail.com>
2015-11-27 01:49:29 +0200

    added friends list and profiles

--- android/TheCookBook/app/src/main/java/com/example/omarali/thecookbook/ProfileActivity.java
+++ android/TheCookBook/app/src/main/java/com/example/omarali/thecookbook/ProfileActivity.java
@@ -4,17 +4,43 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class ProfileActivity extends ActionBarActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        TextView viewFriends = (TextView) this.findViewById(R.id.friends_link_text_view);
+        viewFriends.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), FriendsActivity.class));
+            }
+        });
+
+        TextView postRecipe = (TextView) this.findViewById(R.id.post_recipe_link_text_view);
+        postRecipe.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), CreateRecipeActivity.class));
+            }
+        });
+
+
     }
 
-    void friends_OnClickLink(View v)
-    {
-        startActivity(new Intent(getApplicationContext(), FriendsActivity.class));
-    }
+
+
+
+//    void friends_OnClickLink(View v)
+//    {
+//        startActivity(new Intent(this, FriendsActivity.class));
+//    }
 }
---
+++ android/TheCookBook/app/src/main/java/com/example/omarali/thecookbook/TimelineActivity.java
@@ -0,0 +1,13 @@
+package com.example.omarali.thecookbook;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class TimelineActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+    }
+}
