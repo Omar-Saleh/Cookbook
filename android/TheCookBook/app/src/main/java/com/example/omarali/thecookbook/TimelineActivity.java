b4077fab9eb8cd3c9bda3734941a7c3aafd3e19a
Omar Saleh <omarsaleh0106@gmail.com>
2015-11-28 01:39:41 +0200

    Added post vew

--- android/TheCookBook/app/src/main/java/com/example/omarali/thecookbook/TimelineActivity.java
+++ android/TheCookBook/app/src/main/java/com/example/omarali/thecookbook/TimelineActivity.java
@@ -1,7 +1,9 @@
 package com.example.omarali.thecookbook;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TimelineActivity extends ActionBarActivity {
 
@@ -9,5 +11,9 @@ public class TimelineActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
+    }
+
+    public void imageButtonOnClick(View v) {
+        startActivity(new Intent(getApplicationContext(), ViewPost.class));
     }
 }
